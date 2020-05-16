8282f48fbef78d6ca590267495648e204eca50c4
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-16 10:25:34 +0200

    Create unix_time.py

---
+++ datetime/unix_time.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from PyQt5.QtCore import QDateTime, Qt
+
+now = QDateTime.currentDateTime()
+
+unix_time = now.toSecsSinceEpoch() 
+print(unix_time)
+
+d = QDateTime.fromSecsSinceEpoch(unix_time)
+print(d.toString(Qt.ISODate))
