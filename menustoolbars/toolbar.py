a7937cd676b3b342bfeca2cf55c473200a8966cc
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-16 14:37:51 +0200

    Create toolbar.py

---
+++ menustoolbars/toolbar.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+This program creates a toolbar.
+The toolbar has one action, which
+terminates the application, if triggered.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtGui import QIcon
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAct)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Toolbar')
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
