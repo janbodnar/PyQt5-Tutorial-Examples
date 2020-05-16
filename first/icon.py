1e34288fa87dbcba6316baa7e10439f29fe4cba3
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-16 14:00:22 +0200

    Create icon.py

---
+++ first/icon.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+This example shows an icon
+in the titlebar of the window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
