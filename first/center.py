13b85e16944e9625f7250cf9895e7b571ea00e30
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-16 14:02:52 +0200

    Create center.py

---
+++ first/center.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+This program centers a window
+on the screen.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle('Center')
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
