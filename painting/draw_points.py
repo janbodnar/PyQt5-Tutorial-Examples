a23fc033a440009023b94e8cbd0cb12458278eb7
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-17 13:44:41 +0200

    Create draw_points.py

---
+++ painting/draw_points.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+In the example, we draw randomly 1000 red points
+on the window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt
+import sys, random
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 190)
+        self.setWindowTitle('Points')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
+
+    def drawPoints(self, qp):
+        qp.setPen(Qt.red)
+        size = self.size()
+
+        if size.height() <= 1 or size.height() <= 1:
+            return
+
+        for i in range(1000):
+            x = random.randint(1, size.width() - 1)
+            y = random.randint(1, size.height() - 1)
+            qp.drawPoint(x, y)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
