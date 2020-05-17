59f50f6b37645ad221050bddd6bfc01f7dc8c4f6
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-17 12:25:46 +0200

    Create check_box.py

---
+++ widgets/check_box.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, a QCheckBox widget
+is used to toggle the title of a window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+from PyQt5.QtWidgets import QWidget, QCheckBox, QApplication
+from PyQt5.QtCore import Qt
+import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        cb = QCheckBox('Show title', self)
+        cb.move(20, 20)
+        cb.toggle()
+        cb.stateChanged.connect(self.changeTitle)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('QCheckBox')
+        self.show()
+
+    def changeTitle(self, state):
+
+        if state == Qt.Checked:
+            self.setWindowTitle('QCheckBox')
+        else:
+            self.setWindowTitle(' ')
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
