8d544db2726bbfc0033e1ec453178e7a4c90b2a7
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-17 12:26:12 +0200

    Create slider.py

---
+++ widgets/slider.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+This example shows a QSlider widget.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+from PyQt5.QtWidgets import (QWidget, QSlider,
+                             QLabel, QApplication)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        sld = QSlider(Qt.Horizontal, self)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setGeometry(30, 40, 200, 30)
+        sld.valueChanged[int].connect(self.changeValue)
+
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('mute.png'))
+        self.label.setGeometry(250, 40, 80, 30)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('QSlider')
+        self.show()
+
+    def changeValue(self, value):
+
+        if value == 0:
+
+            self.label.setPixmap(QPixmap('mute.png'))
+        elif 0 < value <= 30:
+
+            self.label.setPixmap(QPixmap('min.png'))
+        elif 30 < value < 80:
+
+            self.label.setPixmap(QPixmap('med.png'))
+        else:
+
+            self.label.setPixmap(QPixmap('max.png'))
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
